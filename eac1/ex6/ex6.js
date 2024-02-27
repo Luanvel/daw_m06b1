209ef4dbfab910e5172ba147891b0d9f7f0866f0
Veronica Ruiz <veronica.ruizab@gmail.com>
2024-02-27 11:26:25 +0100

    first commit

---
+++ eac1/ex6/ex6.js
@@ -0,0 +1,9 @@
+// Funció per afegir un article a la llista
+function afegirItem() {
+    // Escriu aquí el teu codi
+}
+
+// Funció per mostrar la llista
+function mostrarLlista() {
+    // Escriu aquí el teu codi
+}
