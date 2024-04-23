172842ebf9a405b543a71ab426870b92a23ebcdc
Veronica Ruiz <veronica.ruizab@gmail.com>
2024-04-23 14:23:56 +0200

    eac2 enunciat

---
+++ eac2/ex4/ex4.js
@@ -0,0 +1,19 @@
+let cites = {
+    cat: [
+        { frase: "Tria una feina que estimis, i no hauràs de treballar un sol dia de la teva vida.", autor: "Confuci" },
+        { frase: "La pitjor lluita és la que no es fa.", autor: "Karl Marx" },
+        { frase: "Construïm massa murs i no suficients ponts.", autor: "Isaac Newton" },
+        { frase: "La vida és el que passa mentre estàs ocupat fent altres plans.", autor: "John Lennon" }
+    ],
+    esp: [
+        { frase: "La vida es una obra teatral que no importa cuánto haya durado, sino lo bien que haya sido representada.", autor: "Séneca" },
+        { frase: "La victoria más difícil es la victoria sobre uno mismo.", autor: "Aristóteles" },
+        { frase: "La felicidad no brota de la razón, sino de la imaginación.", autor: "Immanuel Kant" }
+    ],
+    eng: [
+        { frase: "Your attitude, not your aptitude, will determine your altitude.", autor: "Zig Ziglar" },
+        { frase: "I have no special talents. I'm just passionately curious.", autor: "Albert Einstein" }
+    ]
+};
+
+// Escriu aquí el teu codi...
