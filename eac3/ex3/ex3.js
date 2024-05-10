3e28dc1f6aae74c401a3eb20920dccb5f8cbe083
Veronica Ruiz <veronica.ruizab@gmail.com>
2024-05-10 20:20:56 +0200

    ex3 eac3

--- eac3/ex3/ex3.js
+++ eac3/ex3/ex3.js
@@ -2,19 +2,105 @@
 
 class Botiga {
     // Escriu aquí el teu codi
+    constructor() {
+        this.productes = [];
+        this.beneficis = 0;
+    }
 
+    getProductes() {
+        return this.productes;
+    }
 
+    getBeneficis() {
+        return this.beneficis;
+    }
+
+    afegirProducte(producte) {
+        this.productes.push(producte);
+    }
+
+    incrementarBeneficis(quantitat) {
+        this.beneficis += quantitat;
+    }
 }
 
 class Producte {
     // Escriu aquí el teu codi
+    constructor(nom, preu, quantitatEnEstoc) {
+        this.nom = nom;
+        this.preu = preu;
+        this.quantitatEnEstoc = quantitatEnEstoc;
+        this.botiga = null;
+    }
 
+    getNom() {
+        return this.nom;
+    }
 
+    setNom(nom) {
+        this.nom = nom;
+    }
+
+    getPreu() {
+        return this.preu;
+    }
+
+    setPreu(preu) {
+        this.preu = preu;
+    }
+
+    getquantitatEnEstoc() {
+        return this.quantitatEnEstoc;
+    }
+
+    setBotiga(botiga) {
+        this.botiga = botiga;
+    }
+
+    vendre(quantitat) {
+        if (this.quantitatEnEstoc > 0 && this.quantitatEnEstoc >= quantitat) {
+            this.quantitatEnEstoc = this.quantitatEnEstoc - quantitat;
+            this.botiga.incrementarBeneficis(quantitat);
+        }
+    }
+
+    reabastir(quantitat) {
+        if (quantitat < 0) {
+            console.log("No es pot reabastir una quantitat negativa.");
+            return;
+        }
+        this.quantitatEnEstoc += quantitat;
+    }
 }
 
 // Escriu aquí el teu codi per les classes ProducteAlimentari i ProducteElectronic
 
+class ProducteAlimentari extends Producte {
+    constructor(nom, preu, quantitatEnEstoc, dataCaducitat) {
+        super(nom, preu, quantitatEnEstoc);
+        this.dataCaducitat = dataCaducitat;
+    }
 
+    comprovarCaducitat() {
+        return (this.dataCaducitat < new Date());
+    }
+}
+
+class ProducteElectronic extends Producte {
+    constructor(nom, preu, quantitatEnEstoc, garantia) {
+        super(nom, preu, quantitatEnEstoc);
+        this.garantia = garantia
+    }
+
+    calcularPreuDescompte() {
+        if (this.garantia > 1) {
+            return this.preu = this.preu - (0.1 * this.preu);
+        } else {
+            return this.preu;
+        }
+    }
+
+}
 
 
 
